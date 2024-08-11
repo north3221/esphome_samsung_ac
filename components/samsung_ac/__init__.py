d40c17633dcd960b5e139e883264979e8b4d2a7c
north3221 <north3221@gmail.com>
2024-08-11 22:30:04 +0100

    added binary sensor import

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor, switch, select, number, climate
+from esphome.components import uart, sensor, switch, select, number, climate, binary_sensor
 from esphome.const import (
     CONF_ID,
     DEVICE_CLASS_TEMPERATURE,
@@ -24,7 +24,7 @@ from esphome.core import CORE, Lambda
 
 CODEOWNERS = ["matthias882", "lanwin", "omerfaruk-aran"]
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["sensor", "switch", "select", "number", "climate"]
+AUTO_LOAD = ["sensor", "switch", "select", "number", "climate", "binary_sensor"]
 MULTI_CONF = False
 
 CONF_SAMSUNG_AC_ID = "samsung_ac_id"
@@ -141,7 +141,7 @@ CUSTOM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
     }
 )
 
-CUSTOM_BINARY_SENSOR_SCHEMA = sensor.binary_sensor_schema().extend({
+CUSTOM_BINARY_SENSOR_SCHEMA = binary_sensor.binary_sensor_schema().extend({
     cv.Required(CONF_DEVICE_CUSTOM_MESSAGE): cv.hex_int,
 })
 
