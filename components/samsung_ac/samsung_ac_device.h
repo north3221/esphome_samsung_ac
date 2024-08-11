79c0b1cf607b3ba3f42ef6be43dc12d56cdee566
north3221 <north3221@gmail.com>
2024-08-11 23:05:22 +0100

    fixing missing

--- components/samsung_ac/samsung_ac_device.h
+++ components/samsung_ac/samsung_ac_device.h
@@ -96,7 +96,7 @@ namespace esphome
     struct Samsung_AC_Binary_Sensor
     {
       uint16_t message_number;
-      binary_sensor::Binary_Sensor *sensor;
+      binary_sensor::BinarySensor *binary_sensor;
     };
 
     class Samsung_AC_Device
@@ -189,7 +189,7 @@ namespace esphome
         indoor_eva_in_temperature = sensor;
       }
       
-      void add_custom_binary_sensor(int message_number, binary_sensor::Binary_Sensor *binary_sensor)
+      void add_custom_binary_sensor(int message_number, binary_sensor::BinarySensor *binary_sensor)
       {
         Samsung_AC_Binary_Sensor cust_binary_sensor;
         cust_binary_sensor.message_number = (uint16_t)message_number;
